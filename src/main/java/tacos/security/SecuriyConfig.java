4d49c10610d154b2bac28c80d88e13c63fbe0b4a
Quan Truong <cloudmadeofcandy@gmail.com>
2025-12-17 17:54:38 +0700

    feat(chap5.2): config authen

---
+++ src/main/java/tacos/security/SecuriyConfig.java
@@ -0,0 +1,40 @@
+package tacos.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import tacos.data.UserRepository;
+import tacos.model.User;
+
+@Configuration
+public class SecuriyConfig {
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+//    @Bean
+//    public UserDetailsService userDetailsService(PasswordEncoder encoder) {
+//        List<UserDetails> usersList = new ArrayList<>();
+//        usersList.add(
+//                new User("buzz", encoder.encode("password"),
+//                        Arrays.asList(new SimpleGrantedAuthority("ROLE_USER"))));
+//        usersList.add(
+//                new User("woody", encoder.encode("password"),
+//                        Arrays.asList(new SimpleGrantedAuthority("ROLE_USER"))));
+//        return new InMemoryUserDetailsManager(usersList);
+//    }
+
+    @Bean
+    public UserDetailsService userDetailsService(UserRepository userRepo) {
+        return username -> {
+            User user = userRepo.findByUsername(username);
+            if (user != null) return user;
+
+            throw new UsernameNotFoundException("User " + username + " not found");
+        };
+    }
+}
